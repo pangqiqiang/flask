5eb307c1dea575cee706a9ca6a319c92733a1262
pangqiqiang <pangqiqiang1234@163.com>
2018-05-13 11:04:48 +0800

    add currency form

--- headlines.py
+++ headlines.py
@@ -10,16 +10,14 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             '36kr': 'http://36kr.com/feed',
             'chong4': 'http://www.chong4.com.cn/feed.php',
             'iol': 'http://www.iol.co.za/cmlink/1.640'}
-DEFAULTS = {"publication": "bcc", "city": "北京", 
+DEFAULTS = {"publication": "bbc", "city": "北京", 
             "currency_from": "USD", "currency_to": "CNY"}
-WEATHER_URL = "https://www.sojson.com/open/api/weather/json.shtml/"
+WEATHER_URL = "https://www.sojson.com/open/api/weather/json.shtml"
 CURRENCY_URL = "http://api.k780.com/"
-APPKEY="10003"
-SIGN="b59bc3ef6191eb9f747dd4e83c99f2a4"
 
-def get_weather(query):
-    query = quote(query)
-    api_url = WEATHER_URL + "?city=" + query
+
+def get_weather(city):
+    api_url = WEATHER_URL + "?city=" + city
     r = requests.get(api_url)
     if r.status_code == 200:
         data = r.json()
@@ -27,7 +25,6 @@ def get_weather(query):
         raw_weather = data.get("data")
         weather = None
         if raw_weather and data.get("city"):
-            raw_weather = raw_weather
             weather = {"city": data.get("city"),
                         "wendu": raw_weather.get("wendu"),
                         "shidu": raw_weather.get("shidu"),
@@ -36,9 +33,9 @@ def get_weather(query):
                          }
         return weather
 
-def get_rate(frm=None, to=None):
-    if not frm or not to:
-        frm, to = DEFAULTS["currency_from"], DEFAULTS["currency_to"]
+def get_rate(frm, to):
+    APPKEY="10003"
+    SIGN="b59bc3ef6191eb9f747dd4e83c99f2a4"
     queries = "?app={app}&scur={frm}&tcur={to}&appkey={appkey}&sign={sign}".format(
         app="finance.rate", frm=frm.upper(), to=to.upper(), appkey=APPKEY, sign=SIGN)
     api_url = CURRENCY_URL + queries
@@ -49,25 +46,29 @@ def get_rate(frm=None, to=None):
     return rate
 
 
-def get_news(query):
-    if query.lower() not in RSS_FEEDS:
-         query = DEFAULTS["publication"] 
-    feed = feedparser.parse(RSS_FEEDS[query])
+def get_news(publication):
+    if publication.lower() not in RSS_FEEDS:
+         publication = DEFAULTS["publication"] 
+    feed = feedparser.parse(RSS_FEEDS[publication])
     return feed.get("entries")   
 
 @app.route("/")
 def home():
     publication = request.args.get("publication")
     city = request.args.get("city")
-    DEFAULTS={"publication": "bbc", "city": "成都"}
+    currency_from = request.args.get("currency_from")
+    currency_to = request.args.get("currency_to")
     if not publication:
         publication = DEFAULTS["publication"]
     articles = get_news(publication)
     if not city:
         city = DEFAULTS["city"]
     weather = get_weather(city)
-    return render_template("home.html",articles=articles, weather=weather)
+    if not currency_from or not currency_to:
+        currency_from, currency_to = DEFAULTS["currency_from"], DEFAULTS["currency_to"]
+    rate = get_rate(currency_from, currency_to)
+
+    return render_template("home.html",articles=articles, weather=weather, currency_from=currency_from, currency_to=currency_to, rate=rate)
 
 if __name__ == "__main__":
-    #app.run(port=5000, debug=True)
-    print(get_rate())
+    app.run(port=5000, debug=True)
